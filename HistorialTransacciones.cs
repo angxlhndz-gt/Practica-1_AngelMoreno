384eb165bae0e79cf999b1a698eacc174c8d1785
Angel Hernández <angelgt12.net@gmail.com>
2025-03-26 22:41:15 -0600

    HistorialTransacciones.cs

--- HistorialTransacciones.cs
+++ HistorialTransacciones.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +14,13 @@ namespace practica_progra
 
         public static void Registrar(string fechaHora, string boveda, string tipo, int monto)
         {
-            // Crear el archivo si no existe y agregar encabezados
+            
             if (!File.Exists(filePath))
             {
                 File.AppendAllText(filePath, "Fecha,Bóveda,Tipo de Transacción,Valor\n");
             }
 
-            // Registrar la transacción en el archivo
+            
             string linea = $"{fechaHora},{boveda},{tipo},{monto}";
             File.AppendAllText(filePath, linea + "\n");
         }
